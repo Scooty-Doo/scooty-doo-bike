deb9e6b5629a6484145679aa8b8338095ba98a25
William Matkaselkä <william.matkaselka@hotmail.com>
2025-01-09 00:03:08 +0100

    changing default speed to 100 kmh for simulation development

--- src/_utils/_settings.py
+++ src/_utils/_settings.py
@@ -7,7 +7,7 @@ class Settings:
         default_latitude = float(0.0)
 
     class Speed:
-        default_speed_limit = 20
+        default_speed_limit = 100
 
     class Battery:
         drain_per_minute = 0.05
