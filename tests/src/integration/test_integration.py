d6e943f56992910604854d5d633eb169c7175e0d
William Matkaselkä <william.matkaselka@hotmail.com>
2025-01-13 23:45:54 +0100

    f

--- tests/src/integration/test_integration.py
+++ tests/src/integration/test_integration.py
@@ -165,7 +165,9 @@ class TestIntegration:
         end_reponse_text = end_response.json()
         is_moving_text = "Bike is moving"
 
-        assert end_response.status_code == 200 or is_moving_text in end_reponse_text
+        assert end_response.status_code == 200 or is_moving_text in str(end_reponse_text)
+        if end_response.status_code == 200:
+            assert end_response.json() == self.correct_response_end
 
     @pytest.mark.usefixtures("integration_mock_fixture")
     def test_whole_trip_no_move(self):
