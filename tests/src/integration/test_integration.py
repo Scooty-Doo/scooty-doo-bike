63239ece6a9fb31e1a0087cf334a4f0c846369c8
William Matkaselkä <william.matkaselka@hotmail.com>
2025-01-13 23:35:39 +0100

    fix

--- tests/src/integration/test_integration.py
+++ tests/src/integration/test_integration.py
@@ -162,10 +162,10 @@ class TestIntegration:
         if end_response.status_code != 200:
             print(end_response.json())
 
-        end_response = end_response.json()
+        end_reponse_text = end_response.json()
         expected_end_response_text = "{'detail': 'Internal Server Error. Details: Bike is moving or charging and cannot accept further requests until moving or charging is completed.'}"
 
-        assert end_response.status_code == 200 or str(end_response) == expected_end_response_text
+        assert end_response.status_code == 200 or str(expected_end_response_text) == end_reponse_text
         assert end_response.json() == self.correct_response_end
 
     @pytest.mark.usefixtures("integration_mock_fixture")
